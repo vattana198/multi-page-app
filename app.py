2b4b50b3365eea79698ee89d5ae315a23540f92d
Your Name <elnuraliyev1@gmail.com>
2022-06-03 21:54:24 -0500

    update2

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ app = MultiApp()
 st.markdown("""
 # Multi-Page App
 
-This multi-page app is using the [streamlit-multiapps](https://github.com/upraneelnihar/streamlit-multiapps) framework developed by [Praneel Nihar](https://medium.com/@u.praneel.nihar). Also check out his [Medium article](https://medium.com/@u.praneel.nihar/building-multi-page-web-app-using-streamlit-7a40d55fa5b4).
-
+This multi-page app is using the [streamlit-multiapps]
 """)
 
 # Add all your application here
