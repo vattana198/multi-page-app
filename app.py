b80bb2b1def9290f9c01face33d892e85fd2ad39
Your Name <elnuraliyev1@gmail.com>
2022-06-03 22:18:24 -0500

    update2

--- app.py
+++ app.py
@@ -14,5 +14,7 @@ This multi-page app is using the [streamlit-multiapps]
 app.add_app("Home", home.app)
 app.add_app("Data", data.app)
 app.add_app("Model", model.app)
+
+app.add_app("Elnur", eln.app)
 # The main app
 app.run()
